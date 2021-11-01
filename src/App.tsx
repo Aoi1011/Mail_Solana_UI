739b6ca08d730f8d852736d6b8278bb7cae5f13a
Aoi <ht.so.ik0102@gmail.com>
2021-11-01 22:52:32 +0700

    update Navbar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+
+      <Navbar />
     </ThemeProvider>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,37 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import { AppBar, Box, Icon, IconButton, Toolbar } from "@mui/material";
+import { Menu as MenuIcon } from "@mui/icons-material";
+
+const drawerWidth = 240;
 
 const Navbar = () => {
-  return <Box></Box>;
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  return (
+    <Box sx={{ display: "flex" }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open-drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Navbar;
