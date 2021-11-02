51a08519d308caeb4d2fb3e0d482cb8a913a908c
Aoi <ht.so.ik0102@gmail.com>
2021-11-02 07:37:19 +0700

    add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-
-      <Navbar />
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Switch>
+          <Route path="/" component={Navbar} />
+        </Switch>
+      </ThemeProvider>
+    </Router>
   );
 }
 
