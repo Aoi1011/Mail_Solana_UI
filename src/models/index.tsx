be00fef49e3a46d9abcd28d157a1d763287e3791
Aoi <ht.so.ik0102@gmail.com>
2021-11-04 06:55:13 +0700

    create store.ts

--- src/models/index.tsx
+++ src/models/index.tsx
@@ -1,13 +1,55 @@
 import { deserialize, serialize } from "borsh";
 
 class Assignable {
-    constructor(properties:  any) {
-        Object.keys(properties).forEach((key) => {
-            this[key] = properties[key];
-        });
-    }
+  constructor(properties: any) {
+    Object.keys(properties).forEach((key) => {
+      this[key as keyof Assignable] = properties[key];
+    });
+  }
 
-    encode() {
-        return serialize(SCHEMA, this);
-    }
+  encode() {
+    return serialize(SCHEMA, this);
+  }
 }
+
+export class Mail extends Assignable {
+  static decode(bytes: any) {
+    return deserialize(SCHEMA, Mail, bytes);
+  }
+}
+
+export class MailAccount extends Assignable {
+  static decode(bytes: any) {
+    const dataLengthBuffer = Buffer.alloc(4);
+    bytes.copy(dataLengthBuffer, 0, 0, 4);
+
+    const dataLength = DataLength.decode(dataLengthBuffer);
+
+    const accountDataBuffer = Buffer.alloc(dataLength.length);
+    bytes.copy(accountDataBuffer, 0, 4, dataLength.length);
+    return deserialize(SCHEMA, MailAccount, accountDataBuffer);
+  }
+}
+
+export class DataLength extends Assignable {
+  static decode(bytes: any) {
+    return deserialize(SCHEMA, DataLength, bytes);
+  }
+}
+
+const SCHEMA = new Map([
+  [
+    Mail,
+    {
+      kind: "struct",
+      fields: [
+        ["id", "string"],
+        ["fromAddress", "string"],
+        ["toAddress", "string"],
+        ["subject", "string"],
+        ["body", "string"],
+        ["sentDate", "string"],
+      ],
+    },
+  ],
+]);
