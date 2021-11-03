def33e5ed1a2549104bc0a49c8f5b76a1604585c
Aoi <ht.so.ik0102@gmail.com>
2021-11-03 07:32:14 +0700

    create mailtoolbar.tsx

--- src/components/MailToolbar.tsx
+++ src/components/MailToolbar.tsx
@@ -1,21 +1,29 @@
-import { Toolbar } from "@mui/material";
 import React from "react";
+import { alpha, IconButton, Toolbar, Tooltip, Typography } from "@mui/material";
+import {
+  Delete as DeleteIcon,
+  FilterList as FilterListIcon,
+} from "@mui/icons-material";
 
 const MailToolbar = (props: any) => {
-    return (
-        <Toolbar
+  const { numSelected } = props;
+  return (
+    <Toolbar
       sx={{
         pl: { sm: 2 },
         pr: { xs: 1, sm: 1 },
         ...(numSelected > 0 && {
           bgcolor: (theme) =>
-            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-        })
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity
+            ),
+        }),
       }}
     >
       {numSelected > 0 ? (
         <Typography
-          sx={{ flex: '1 1 100%' }}
+          sx={{ flex: "1 1 100%" }}
           color="inherit"
           variant="subtitle1"
           component="div"
@@ -24,7 +32,7 @@ const MailToolbar = (props: any) => {
         </Typography>
       ) : (
         <Typography
-          sx={{ flex: '1 1 100%' }}
+          sx={{ flex: "1 1 100%" }}
           variant="h6"
           id="tableTitle"
           component="div"
@@ -47,7 +55,7 @@ const MailToolbar = (props: any) => {
         </Tooltip>
       )}
     </Toolbar>
-    )
+  );
 };
 
 export default MailToolbar;
