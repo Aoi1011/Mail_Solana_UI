ba787ccb8235b17c253c768d66e1a9cd6914c5e0
Aoi <ht.so.ik0102@gmail.com>
2021-11-04 07:39:40 +0700

    create store

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,22 @@ import theme from "./theme";
 import Navbar from "./components/Navbar";
 import Signin from "./pages/loggedout/Signin";
 import Main from "./pages/loggedin";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <Router>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Switch>
-          {/* <Route path="/" component={Navbar} /> */}
-          <Route path="/mail" exact component={Main} />
-          <Route path="/" exact component={Signin} />
-        </Switch>
-      </ThemeProvider>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Switch>
+            <Route path="/mail" exact component={Main} />
+            <Route path="/" exact component={Signin} />
+          </Switch>
+        </ThemeProvider>
+      </Router>
+    </Provider>
   );
 }
 
