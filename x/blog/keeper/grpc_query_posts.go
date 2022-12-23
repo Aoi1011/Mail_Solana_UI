eea04d5845da6618e0777d9a3cf00eb94958dee5
Aoi1011 <ht.so.ik0102@gmail.com>
2022-12-23 10:50:52 +0700

    Add a comment

--- x/blog/keeper/grpc_query_posts.go
+++ x/blog/keeper/grpc_query_posts.go
@@ -13,18 +13,24 @@ import (
 )
 
 func (k Keeper) Posts(c context.Context, req *types.QueryPostsRequest) (*types.QueryPostsResponse, error) {
+	// Throw an error if request is nil
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
+	// Define a variable that will store a list of posts
 	var posts []*types.Post
 
+	// Get context with the information about the environment
 	ctx := sdk.UnwrapSDKContext(c)
 
+	// Get the key-value module store using the store key (in our case store key is "chain")
 	store := ctx.KVStore(k.storeKey)
 
+	// Get the part of the store that keeps posts (using post key, which is "Post-value-")
 	postStore := prefix.NewStore(store, []byte(types.PostKey))
 
+	// Paginate the posts store based on PageRequest
 	pageRes, err := query.Paginate(postStore, req.Pagination, func(key, value []byte) error {
 		var post types.Post
 		if err := k.cdc.Unmarshal(
@@ -38,9 +44,11 @@ func (k Keeper) Posts(c context.Context, req *types.QueryPostsRequest) (*types.Q
 		return nil
 	})
 
+	// Throw an error if pagination failed
 	if err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
 
+	// Return a struct containing a list of posts and pagination info
 	return &types.QueryPostsResponse{Post: posts, Pagination: pageRes}, nil
 }
