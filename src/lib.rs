643cb54036248566392d0d0aa9692c98b8a50b15
Aoi1011 <ht.so.ik0102@gmail.com>
2023-02-23 22:00:02 +0900

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::net::SocketAddr;
+
+use tokio::prelude::*;
+use failure;
+
+pub struct Zookeeper {}
+
+impl Zookeeper {
+    pub fn connect(addr: &SocketAddr) -> impl Future<Item = Self, Error = failure::Error> {
+        tokio::net::TcpStream::connect(addr).and_then(|stream| {
+            Self::handshake(stream);
+        })
+    }
+
+    fn handshake(stream: tokio::net::TcpStream) -> impl Future<Item = Self, Error = failure::Error> {
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+        use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        #[test]
+        fn it_works() {
+            let zk = tokio::run(Zookeeper::connect());
+        }
     }
-}
+
