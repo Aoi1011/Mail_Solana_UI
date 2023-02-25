12450735b8809bb37af257c8d2a97660951bcf05
Aoi1011 <ht.so.ik0102@gmail.com>
2023-02-25 16:59:52 +0900

    Continue implementing

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,53 @@
 use std::net::SocketAddr;
 
-use tokio::prelude::*;
 use failure;
+use proto::Packetizer;
+use tokio::prelude::*;
 
 mod proto;
 
-pub struct Zookeeper {}
+pub struct Zookeeper<S> {
+    connection: Packetizer<S>,
+}
 
-impl Zookeeper {
+impl Zookeeper<S> {
     pub fn connect(addr: &SocketAddr) -> impl Future<Item = Self, Error = failure::Error> {
         tokio::net::TcpStream::connect(addr).and_then(|stream| {
             Self::handshake(stream);
         })
     }
 
-    fn handshake(stream: tokio::net::TcpStream) -> impl Future<Item = Self, Error = failure::Error> {
-        let request = proto::Connection {};
-        let stream = proto::wrap(stream);
-        stream.send(request).and_then(|stream| {
-            stream.receive();
-        }).and_then(|(response, stream)| {
-                Zookeeper {}
+    fn handshake(
+        stream: tokio::net::TcpStream,
+    ) -> impl Future<Item = Self, Error = failure::Error> {
+        let request = proto::Request::Connect {
+            protocol_version: 0,
+            last_zxid_seen: 0,
+            timeout: 0,
+            session_id: 0,
+            passwd: vec![],
+            read_only: false,
+        };
+        Packetizer::new(stream)
+            .send(request)
+            .and_then(|zk| {
+                zk.into_future();
+            })
+            .map(|(response, zk)| {
+                if response.is_none() {
+                    unimplemented!();
+                }
+                Zookeeper { connection: zk }
             })
     }
 }
 
 #[cfg(test)]
 mod tests {
-        use super::*;
+    use super::*;
 
-        #[test]
-        fn it_works() {
-            let zk = tokio::run(Zookeeper::connect());
-        }
+    #[test]
+    fn it_works() {
+        let zk = tokio::run(Zookeeper::connect());
     }
-
+}
